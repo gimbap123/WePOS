5c92d6872510429220e9f4e0d054f68025644234
wan <slayers0250@naver.com>
2017-03-20 14:28:21 +0900

    스크립트 수정

--- WePOS/src/main/java/com/wepos/common/controller/CommonController.java
+++ WePOS/src/main/java/com/wepos/common/controller/CommonController.java
@@ -75,8 +75,8 @@ public class CommonController {
 			result="이미 등록된 아이디입니다.";
 		else if(res==0)
 			result="사용 가능한 아이디입니다.";
-		mav.addObject("res",res);
 		
+		mav.addObject("res",res);		
 		mav.addObject("result",result.trim());
 		mav.setViewName("common/checkId");
 			
