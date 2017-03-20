df1178ac5ef345263a7a044d492b9c63e8abac86
daehan lee <gimbap123@naver.com>
2017-03-20 14:32:09 +0900

    스크립트 수정

--- WePOS/src/main/webapp/view/common/script.js
+++ WePOS/src/main/webapp/view/common/script.js
@@ -41,62 +41,7 @@ function checkWrite(){
 		document.boardWrite.submit();
 	}
 
-
-function check(){
-		if(document.regForm.user_name.value=="" || document.regForm.user_name.value==null){
-			alert("이름을 입력하세요");
-			document.regForm.user_name.focus();
-			return false;
-		}
-		if(document.regForm.user_id.value=="" || document.regForm.user_id.value==null){
-			alert("아이디를 입력하세요");
-			document.regForm.user_id.focus();
-			return false;
-		}
-		if(document.regForm.user_password.value=="" || document.regForm.user_password.value==null){
-			alert("암호를 입력하세요");
-			document.regForm.user_password.focus();
-			return false;
-		}
-		if(document.regForm.user_password2.value=="" || document.regForm.user_password2.value==null){
-			alert("암호확인을 입력하세요");
-			document.regForm.user_password2.focus();
-			return false;
-		}
-		if(document.regForm.user_phone.value=="" || document.regForm.user_phone.value==null){
-			alert("연락처를 입력하세요");
-			document.regForm.user_phone.focus();
-			return false;
-		}
-		if(document.regForm.user_email.value=="" || document.regForm.user_email.value==null){
-			alert("이메일을 입력하세요");
-			document.regForm.user_email.focus();
-			return false;
-		}
-		else
-			checkPwd();		
-	}
-
-function checkWrite()
-{
-	if(document.boardWrite.boardTitle.value=="" || document.boardWrite.boardTitle.value==null)
-	{
-		alert("제목을 입력하세요");
-		document.boardWrite.boardTitle.focus();
-		return false;
-	}		
-	if(document.boardWrite.boardContent.value=="" || document.boardWrite.boardContent.value==null)
-	{
-		alert("내용을 입력하세요");
-		document.boardWrite.boardContent.focus();
-		return false;
-	}
-	
-	alert("글이 성공적으로 등록되었습니다!");
-	document.boardWrite.submit();
-}
-
-
+// 회원가입
 function check()
 {
 	if(document.regForm.userName.value=="" || document.regForm.userName.value==null)
