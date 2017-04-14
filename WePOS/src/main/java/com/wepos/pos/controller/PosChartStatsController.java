f86ab647f2aab52dee42069ec288335098c7f49b
daehan lee <gimbap123@naver.com>
2017-04-14 12:26:12 +0900

    차트 통계 구현중

--- WePOS/src/main/java/com/wepos/pos/controller/PosChartStatsController.java
+++ WePOS/src/main/java/com/wepos/pos/controller/PosChartStatsController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,7 @@ public class PosChartStatsController {
 	}
 	
 	// 차트 통계 기능 수행
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/pos/posChartStats.do", method = RequestMethod.POST)
 	public ModelAndView posChartStatsProcess(@RequestParam("shopCode") int shopCode, 
 			@RequestParam("chartType") int chartType, @RequestParam("start") String start, 
@@ -48,8 +51,40 @@ public class PosChartStatsController {
 			chartStatsList = posChartStatsDao.monthStats(map);
 		}	
 		
+		JSONObject jsonChartData = new JSONObject();
+		JSONArray labels = new JSONArray();
+		JSONArray datasets = new JSONArray();
+		JSONArray dataArray = new JSONArray();
+		
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("label", "총 매출");
+		jsonData.put("fillColor", "rgba(210, 214, 222, 1)");
+		jsonData.put("strokeColor", "rgba(210, 214, 222, 1)");
+		jsonData.put("pointColor", "rgba(210, 214, 222, 1)");
+		jsonData.put("pointStrokeColor", "#c1c7d1");
+		jsonData.put("pointHighlightFill", "#fff");
+		jsonData.put("pointHighlightStroke", "rgba(220,220,220,1)");
+		
+		for(ChartStatsDto chartStats : chartStatsList)
+		{
+			labels.add(chartStats.getMon() + "월");		
+				
+			dataArray.add(chartStats.getTotalPrice());		
+			
+		}
+		
+		jsonData.put("data", dataArray);
+		datasets.add(jsonData);	
+		
+		jsonChartData.put("labels", labels);
+		jsonChartData.put("datasets", datasets);	
+		
+		String chartTitle = "월별 매출 (" + start + " ~ " + finish + ")";
+		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("pos/posChartStats");
+		mav.addObject("jsonChartData", jsonChartData);
+		mav.addObject("chartTitle", chartTitle);
 			
 		return mav;
 	}
