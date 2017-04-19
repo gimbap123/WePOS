381bbabd38fcf3254840108ec5e3475c65d5ea65
daehan lee <gimbap123@naver.com>
2017-04-19 14:38:25 +0900

    상품 리스트에서 평균 평점 추가

--- WePOS/src/main/java/com/wepos/common/dto/ProductDto.java
+++ WePOS/src/main/java/com/wepos/common/dto/ProductDto.java
@@ -3,83 +3,92 @@ package com.wepos.common.dto;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ProductDto {
-  private int productCode;
-  private int shopCode;
-  private int categoryCode;
-  private String categoryName;
-  private String productName;
-  private int productPrice;
-  private int productStock;
-  private String productDesc;
-  private String productFile;
-  private MultipartFile upload;
-  private int countOrder;
-  
-  public int getProductCode() {
-    return productCode;
-  }
-  public void setProductCode( int productCode ) {
-    this.productCode = productCode;
-  }
-  public int getShopCode() {
-    return shopCode;
-  }
-  public void setShopCode( int shopCode ) {
-    this.shopCode = shopCode;
-  }
-  public int getCategoryCode() {
-    return categoryCode;
-  }
-  public void setCategoryCode( int categoryCode ) {
-    this.categoryCode = categoryCode;
-  }
-  public String getCategoryName() {
-	return categoryName;
-}
-public void setCategoryName(String categoryName) {
-	this.categoryName = categoryName;
-}
-public String getProductName() {
-    return productName;
-  }
-  public void setProductName( String productName ) {
-    this.productName = productName;
-  }
-  public int getProductPrice() {
-    return productPrice;
-  }
-  public void setProductPrice( int productPrice ) {
-    this.productPrice = productPrice;
-  }
-  public int getProductStock() {
-    return productStock;
-  }
-  public void setProductStock( int productStock ) {
-    this.productStock = productStock;
-  }
-  public String getProductDesc() {
-    return productDesc;
-  }
-  public void setProductDesc( String productDesc ) {
-    this.productDesc = productDesc;
-  }
-  public String getProductFile() {
-	return productFile;
-  }
-  public void setProductFile(String productFile) {
-	this.productFile = productFile;
-  }
-  public MultipartFile getUpload() {
-	return upload;
-  }
-  public void setUpload(MultipartFile upload) {
-	this.upload = upload;
-  }
-public int getCountOrder() {
-	return countOrder;
-}
-public void setCountOrder(int countOrder) {
-	this.countOrder = countOrder;
-}
-  
+	private int productCode;
+	private int shopCode;
+	private int categoryCode;
+	private String categoryName;
+	private String productName;
+	private int productPrice;
+	private int productStock;
+	private String productDesc;
+	private String productFile;
+	private MultipartFile upload;
+	private int countOrder;
+
+	// 평균 평점을 위한 멤버 변수 추가
+	private int avgGrade;
+
+	public int getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(int productCode) {
+		this.productCode = productCode;
+	}
+	public int getShopCode() {
+		return shopCode;
+	}
+	public void setShopCode(int shopCode) {
+		this.shopCode = shopCode;
+	}
+	public int getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(int categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+	public int getProductStock() {
+		return productStock;
+	}
+	public void setProductStock(int productStock) {
+		this.productStock = productStock;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public String getProductFile() {
+		return productFile;
+	}
+	public void setProductFile(String productFile) {
+		this.productFile = productFile;
+	}
+	public MultipartFile getUpload() {
+		return upload;
+	}
+	public void setUpload(MultipartFile upload) {
+		this.upload = upload;
+	}
+	public int getCountOrder() {
+		return countOrder;
+	}
+	public void setCountOrder(int countOrder) {
+		this.countOrder = countOrder;
+	}
+	public int getAvgGrade() {
+		return avgGrade;
+	}
+	public void setAvgGrade(int avgGrade) {
+		this.avgGrade = avgGrade;
+	}
+
 }
--- WePOS/src/main/webapp/view/common/productListTab.jsp
+++ WePOS/src/main/webapp/view/common/productListTab.jsp
@@ -49,12 +49,24 @@
 	                           <li><i class="fa fa-krw"></i> <fmt:formatNumber value="${product.productPrice}" type="number" />원</li>	                          
 	                       </ul>
 	                       <p>${product.productDesc}</p>
+	                      
+	                       <ul class="list-inline star-vote" style="padding-left: 5px">
+	                       		<c:forEach var="i" begin="1" end="5" step="1">
+	                       			<c:if test="${product.avgGrade >= i}">
+	                       				<li style="margin: 0px; padding: 0px;"><i class="color-green fa fa-star"></i></li>											                       			
+	                       			</c:if>
+	                       			<c:if test="${product.avgGrade < i}">
+	                       				<li style="margin: 0px; padding: 0px;"><i class="color-green fa fa-star-o"></i></li>											                       			
+	                       			</c:if>
+	                       		</c:forEach>                                                          
+                            </ul>
+                           
 	                       <div class="row" style="padding-left: 15px">
 		                       <button class="btn-u" type="button" data-toggle="modal" data-target="#productGradeModal" onclick="productGradeModal(${product.productCode}, '${product.productName}')">평점 보기</button>
 		                       <c:if test="${sessionScope.id != null && sessionScope.userType != 2}">
 		                       		<button class="btn-u" type="button" onclick="productGradeView(${product.productCode}, true)">평점 주기</button>
 		                       </c:if>
-	                       </div>
+	                       </div>	                       
                     	</div>
                     	<div class="col-md-5" id="userGradeDiv${product.productCode}" style="display: none;">
                     		
